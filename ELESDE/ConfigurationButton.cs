bb941df078acf805fce7b273dce216f3f40b5410
Gurkenschreck <mariogiugno@gmx.de>
2015-04-20 12:40:50 +0200

    Added button frame

---
+++ ELESDE/ConfigurationButton.cs
@@ -0,0 +1,24 @@
+﻿using ColossalFramework.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ELESDE
+{
+    public class ConfigurationButton : UIButton
+    {
+        int mode = 0; //Current mode of the button
+
+        public override void Start()
+        {
+            base.Start();
+        }
+
+        public override void OnDisable()
+        {
+            base.OnDisable();
+        }
+    }
+}
